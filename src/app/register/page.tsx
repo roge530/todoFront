85f9d9f823a1e188d59726cbd13d3ba0cf4a31c6
roge530 <garu1999@hotmail.com>
2023-06-14 23:59:56 -0600

    Creacion de inicio sesion
    Se creo la pagina de inicio de sesion junto a la pagina de
    registro, se pueden conectar al servidor local de express y
    hacer peticiones post a la base de datos de mongo sin problemas

---
+++ src/app/register/page.tsx
@@ -0,0 +1,97 @@
+'use client'
+import { Button, TextField } from "@mui/material"
+import Link from 'next/link'
+import axios, { AxiosResponse } from "axios"
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+interface FormData {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export default function Register() {
+    const router = useRouter();
+    const [formData, setFormData] = useState<FormData>({
+        name: "",
+        email: "",
+        password: "",
+    });
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          [name]: value,
+        }));
+    };
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        try {
+            const response: AxiosResponse = await axios.post(
+              'http://localhost:3002/users/signUp',
+              formData
+            );
+      
+            if (response.status === 201) router.push("/");
+        } catch (error: any) {
+            console.error(error);
+        }
+    }
+
+    return (
+        <section className="flex items-center justify-center h-screen">
+            <div className="flex justify-between bg-slate-200 flex-col rounded-md items-center justify-center">
+                <div className="p-4">
+                    <h1>Create a new account</h1>
+                </div>
+                <form onSubmit={handleSubmit}>
+                    <div className="p-4">
+                        <TextField
+                            required
+                            label="Name"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="p-4">
+                        <TextField
+                            required
+                            label="Email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="p-4">
+                        <TextField
+                            required
+                            label="Password"
+                            type="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="p-4">
+                        <Button
+                            type="submit"
+                            variant="outlined"
+                            color="primary"
+                            sx={{ backgroundColor: '#000' }}
+                        >
+                            Create account
+                        </Button>
+                        <Button variant="outlined" color="primary" sx={{backgroundColor: '#000'}}>
+                            <Link href="/">Already have an account?</Link>
+                        </Button>
+                    </div>
+                </form>
+            </div>
+        </section>
+    )
+}
---
+++ src/components/logIn.tsx
@@ -0,0 +1,86 @@
+'use client'
+
+import { Button, TextField } from "@mui/material"
+import axios, { AxiosResponse } from "axios";
+import Link from 'next/link'
+import { useState } from "react";
+
+interface FormData {
+    email: string;
+    password: string;
+}
+
+export default function MyLogIn() {
+
+    const [formData, setFormData] = useState<FormData>({
+        email: "",
+        password: "",
+    });
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        try {
+            const response: AxiosResponse = await axios.post(
+                'http://localhost:3002/users/logIn',
+                formData
+            );
+
+            if (response.status === 200) console.log(response.data);
+        } catch (error: any) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <section className="flex items-center justify-center h-screen">
+            <div className="flex justify-between bg-slate-200 flex-col rounded-md items-center justify-center">
+                <div className="p-4">
+                    <h1>Welcome</h1>
+                </div>
+                <form onSubmit={handleSubmit}>
+                    <div className="p-4">
+                        <TextField
+                            required
+                            label="Email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="p-4">
+                        <TextField
+                            required
+                            label="Password"
+                            name="password"
+                            type="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="p-4">
+                        <Button 
+                            type="submit"
+                            variant="outlined"
+                            color="primary"
+                            sx={{backgroundColor: '#000'}}
+                        >
+                            Log In
+                        </Button>
+                        <Button  variant="outlined" color="primary" sx={{backgroundColor: '#000'}}>
+                            <Link href="/register">Register</Link>
+                        </Button>
+                    </div>
+                </form>
+            </div>
+        </section>
+    )
+}
