aa2e9c6d007dd8ec934c830ae907537f04e0a435
roge530 <garu1999@hotmail.com>
2023-06-15 19:05:09 -0600

    inclusion de dashboard y tabla

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import ActivityTable from "@/components/activityTable"
+export default function dashboard() {
+    return (
+        <section>
+            <h1>Welcome </h1>
+            <p>Demo</p>
+            <ActivityTable></ActivityTable>
+        </section>
+    )
+}
