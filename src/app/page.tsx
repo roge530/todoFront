ec92e66e30aea3020c50ad92709c8560697fd831
roge530 <garu1999@hotmail.com>
2023-06-21 02:27:36 -0600

    mejor landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import Link from "next/link"
-
+import HomeMenu from "@/components/homeMenu"
 export default function Home() {
   return (
     <>
-      <h1>Welcome</h1>
-      <button><Link href="/dashboard">Dashboard</Link></button>
-      <button><Link href="/logIn">Log In</Link></button>
-      <button><Link href="/register">Register</Link></button>
+      <HomeMenu/>
     </>
   )
 }
